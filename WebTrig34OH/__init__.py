db08c3cb4e595522945db51ffa05686d10490f28
Nizar El Ouarti <nizare@microsoft.com>
2020-05-03 11:29:35 +0000

    2nd commit

--- WebTrig34OH/__init__.py
+++ WebTrig34OH/__init__.py
@@ -14,11 +14,14 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
             pass
         else:
             productId = req_body.get('productId')
+            
 
     if productId:
-        return func.HttpResponse(f"The product name for your product id {productId} is Starfruit Explosion")
+        return func.HttpResponse(productId)
+        
     else:
         return func.HttpResponse(
              "This HTTP triggered function executed successfully. Pass a productId in the query string or in the request body for a personalized response.",
              status_code=200
         )
+       
